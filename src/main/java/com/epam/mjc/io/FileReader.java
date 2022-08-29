e92c1334c80dbaa281109f14102dd8478563f469
Artyom <artvit12@gmail.com>
2022-08-29 20:39:45 +0600

    Solution

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,40 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
+//    public static void main(String[] args) {
+//        System.out.println(System.getProperty("user.home"));
+//        getDataFromFile(new File("src/test/resources/Profile.txt"));
+//    }
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+
+
+    public  Profile getDataFromFile(File file) {
+        String name = "";
+        int age = 9;
+        String email = "";
+        long phone = 0;
+
+        try (FileInputStream in = new FileInputStream(file.getAbsolutePath()) ){
+            int i;
+            String data = "";
+            while ((i = in.read()) != -1){
+                data += (char) i;
+            }
+            String[] splitted = data.split("\n");
+            name = splitted[0].split(": ")[1].replaceAll("\r", "");
+            age = Integer.parseInt(splitted[1].split(": ")[1].replaceAll("\r", ""));
+            email = splitted[2].split(": ")[1].replaceAll("\r", "");
+            phone = Long.parseLong(splitted[3].split(": ")[1].replaceAll("\r", ""));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name,age, email, phone);
     }
 }
